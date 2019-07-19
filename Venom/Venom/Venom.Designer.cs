2e22045653a485732cb2510c24a002e0e5f8ddb5
Matthew Kidder <mattck55@gmail.com>
2019-07-19 14:42:14 -0400

    Initial upload with debug working and ability to read WIN traffic.

---
+++ Venom/Venom/Venom.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace VenomNamespace
+{
+    partial class Venom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TB_Log = new System.Windows.Forms.TextBox();
+            this.TB_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TB_Log
+            // 
+            this.TB_Log.Location = new System.Drawing.Point(87, 101);
+            this.TB_Log.Name = "TB_Log";
+            this.TB_Log.Size = new System.Drawing.Size(100, 20);
+            this.TB_Log.TabIndex = 0;
+            // 
+            // TB_label
+            // 
+            this.TB_label.AutoSize = true;
+            this.TB_label.Location = new System.Drawing.Point(84, 85);
+            this.TB_label.Name = "TB_label";
+            this.TB_label.Size = new System.Drawing.Size(91, 13);
+            this.TB_label.TabIndex = 1;
+            this.TB_label.Text = "First Log...to die...";
+            this.TB_label.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // Venom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Controls.Add(this.TB_label);
+            this.Controls.Add(this.TB_Log);
+            this.Name = "Venom";
+            this.Text = "Venom";
+            this.Load += new System.EventHandler(this.Venom_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TB_Log;
+        private System.Windows.Forms.Label TB_label;
+    }
+}
+
---
+++ Venom/Venom/Venom.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WideBoxLib;
+using WirelessLib;
+
+namespace VenomNamespace
+{
+    public partial class Venom : WideInterface
+    {
+        public const byte API_NUMBER = 0;//TODO: change to API value
+        /// <summary>
+        /// Opcodes parsed by this form
+        /// </summary>
+        public enum OPCODES
+        {
+            //Include your opcodes in here
+            //SET_VALUE = 1
+        }
+
+        //to access wideLocal use base.WideLocal or simple WideLocal
+        public Venom(WideBox wideLocal, WhirlpoolWifi wifiLocal)
+            : base(wideLocal, wifiLocal)
+        {
+            InitializeComponent();
+            //Add your constructor operations here
+
+            //AutoSave: Load and Save your CheckBoxes, TextBoxes and ComboBoxes last conditions
+            //Default is true, this line can be removed
+            AutoSave = true;
+            //To facilitate the debug of your form you can trace your exception or print messages using LogException function
+            //  LogException("My Error Message");
+            //  or
+            //  try { //your code } catch (Exception ex) { LogException(ex,false); }
+            //Avoid to do a to much operations during the construction time to have a faster open time and avoid opening the form errors.
+            //Errors during the construction time are hard to debug because your object are not instantiated yet.
+        }
+
+        /// <summary>
+        /// Parse message from WideBoxInterface
+        /// </summary>
+        /// <param name="data">Simple Whirlpool packet data with extended info (TimeStamp and IsValid)</param>
+        public override void parseSimpleWhirlpoolMessage(ExtendedSimpleWhirlpoolPacket data)
+        {
+            RevealPacket reveal_pkt = new RevealPacket();
+            if (reveal_pkt.ParseSimpleWhirlpoolMessage(data))
+            {
+                string text = reveal_pkt.API.ToString("X2") + "," + reveal_pkt.OpCode.ToString("X2");
+                foreach (byte b in reveal_pkt.PayLoad)
+                {
+                    text += "," + b.ToString("X2");
+                }
+                TB_Log.Text = text;
+
+                switch (reveal_pkt.API)
+                {
+                    case API_NUMBER:   //parse opcodes to this API (already without the feedback bit, i.e. 0x25: reveal_pkt.OpCode = 5 , reveal_pkt.IsFeedback = true )
+                        switch ((OPCODES)reveal_pkt.OpCode)
+                        {
+                            /*
+                             * Create a opcode enumeration to parse specific opcode
+                             * case OPCODES.SET_VALUE:
+                             *     if(reveal_pkt.IsFeedback)
+                             *     {
+                             *          textBox1.Text = reveal_pkt.PayLoad[0].ToString();
+                             *     }
+                             *     break;
+                             *   */
+                            default:
+
+                                break;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Parse messages from the device between the bus and the PC
+        /// </summary>
+        /// <param name="message">string containing a message from the device</param>
+        /// <param name="data">data from the device message</param>
+        public override void parseDeviceMessage(DeviceMessage message)
+        {
+            //Using DeviceMesageString to parse a Device message
+            //Checking if message income is Data Flow State message
+            if (DeviceMessageStrings.CheckDeviceString(DeviceMessageStrings.DataFlowState, message.Message))
+            {
+                //Get the Data from the message into a string list, See DeviceMessageStrings.(message) for info about the data
+                List<string> values = DeviceMessageStrings.GetDeviceStringParameters(wbox.DeviceMessageStrings.DataFlowState, message.Message);
+                //textBoxDataConnected.Text = values[0].ToUpperInvariant() == "ON" ? "Data OFF" : "Data ON";
+            }
+        }
+
+        /// <summary>
+        /// Parse messages from the Revelation connection.
+        /// </summary>
+        /// <param name="data">The data from the revelation connected appliance.</param>
+        public override void parseRevelationMessages(ExtendedRevelationPacket data)
+        {
+            List<RevealPacket> revealpkt_list = new List<RevealPacket>();
+            revealpkt_list.ParseRevelationPacket(data);
+            //A Revelation packet can contain more than one Reveal Message.
+            foreach (RevealPacket reveal_pkt in revealpkt_list)
+            {
+                switch (reveal_pkt.API)
+                {
+                    case API_NUMBER:   //parse opcodes to this API (already without the feedback bit, i.e. 0x25: reveal_pkt.OpCode = 5 , reveal_pkt.IsFeedback = true )
+                        switch ((OPCODES)reveal_pkt.OpCode)
+                        {
+                            /*
+                             * Create a opcode enumeration to parse specific opcode
+                             * case OPCODES.SET_VALUE:
+                             *     if(reveal_pkt.IsFeedback)
+                             *     {
+                             *          textBox1.Text = reveal_pkt.PayLoad[0].ToString();
+                             *     }
+                             *     break;
+                             *   */
+                            default:
+
+                                break;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Parse messages from the MQTT connection.
+        /// </summary>
+        /// <param name="data">The data from the mqtt connected appliance.</param>
+        public override void parseMqttMessages(ExtendedMqttMsgPublish data)
+        {
+            RevealPacket reveal_pkt = new RevealPacket();
+            if (reveal_pkt.ParseMqttPacket(data))
+            {
+                switch (reveal_pkt.API)
+                {
+                    case API_NUMBER:   //parse opcodes to this API (already without the feedback bit, i.e. 0x25: reveal_pkt.OpCode = 5 , reveal_pkt.IsFeedback = true )
+                        switch ((OPCODES)reveal_pkt.OpCode)
+                        {
+                            /*
+                             * Create a opcode enumeration to parse specific opcode
+                             * case OPCODES.SET_VALUE:
+                             *     if(reveal_pkt.IsFeedback)
+                             *     {
+                             *          textBox1.Text = reveal_pkt.PayLoad[0].ToString();
+                             *     }
+                             *     break;
+                             *   */
+                            default:
+
+                                break;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Parse messages from the Udp connection.
+        /// </summary>
+        /// <param name="data">The data from the udp connected appliance.</param>
+        public override void parseUdpMessages(ExtendedUdpMessage data)
+        {
+
+        }
+
+        #region WIRED BUS Message functions
+        /// <summary>
+        /// Send a wide Message over the bus
+        /// </summary>
+        /// <param name="destination">Address to send the message</param>
+        /// <param name="sap">sap to transport layer defined in Wide docs (1/2-TDD/UDD 3-Gmcl 4-Reveal)</param>
+        /// <param name="data">wide payload</param>
+        public void SendWideMessage(byte destination, byte sap, byte[] data)
+        {
+            WideLocal.SendWideMsg(destination, sap, data);
+        }
+
+        /// <summary>
+        /// Send a Reveal Message over the bus.
+        /// Packet Source address will be ignored as the WideBox address that will be used
+        /// To set WideBox address send the WideBoxConstants.CMD_GET_WIDE_BOX_ADDR using the WideLocal.SendCommand
+        /// </summary>
+        /// <param name="pkt">Reveal packet containing the command or feedback to be sent</param>
+        public void SendRevealMessage(RevealPacket pkt)
+        {
+            WideLocal.SendMessage(pkt.Destination, WideBoxConstants.REVEAL_SAP, pkt.ToMessagePayload());
+        }
+
+        /// <summary>
+        /// Send a Reveal Message over the bus.
+        /// </summary>
+        /// <param name="destination">Address to send the message</param>
+        /// <param name="api">Reveal Api</param>
+        /// <param name="opcode">Reveal Opcode</param>
+        /// <param name="isFeedback">Set true if the message is a feedback</param>
+        /// <param name="payload">payload bytes</param>
+        public void SendRevealMessage(byte destination, byte api, byte opcode, bool isFeedback, byte[] payload)
+        {
+            RevealPacket pkt = new RevealPacket(api, opcode, 0, destination, isFeedback, payload);
+            WideLocal.SendMessage(pkt.Destination, WideBoxConstants.REVEAL_SAP, pkt.ToMessagePayload());
+        }
+        #endregion
+
+        #region WIRELESS Message functions
+        /// <summary>
+        /// Send a Reveal Message over the Revelation connection.
+        /// </summary>
+        /// <param name="connectedAppliance">The connected Applicance</param>
+        /// <param name="destination">Address to send the message</param>
+        /// <param name="api">Reveal Api</param>
+        /// <param name="opcode">Reveal Opcode</param>
+        /// <param name="isFeedback">Set true if the message is a feedback</param>
+        /// <param name="payload">payload bytes</param>
+        public void SendRevealMessageOverRevelation(ConnectedApplianceInfo connectedAppliance, byte destination, byte api, byte opcode, bool isFeedback, byte[] payload)
+        {
+            RevealPacket pkt = new RevealPacket(api, opcode, 0, destination, isFeedback, payload);
+            WifiLocal.SendRevelationMessage(connectedAppliance, pkt.ToRevelation());
+        }
+
+        /// <summary>
+        /// Send a Reveal Message over the Revelation connection.
+        /// Use WifiLocal.ConnectedAppliances to get the list of current scanned and/or connected appliances.
+        /// </summary>
+        /// <param name="connectedAppliance">The connected Applicance</param>
+        /// <param name="pkt">Reveal packet containing the command or feedback to be sent</param>
+        public void SendRevealMessageOverRevelation(ConnectedApplianceInfo connectedAppliance, RevealPacket pkt)
+        {
+            WifiLocal.SendRevelationMessage(connectedAppliance, pkt.ToRevelation());
+        }
+
+        /// <summary>
+        /// Send a Reveal Message List over the Revelation connection.
+        /// Use WifiLocal.ConnectedAppliances to get the list of current scanned and/or connected appliances.
+        /// </summary>
+        /// <param name="connectedAppliance">The connected Applicance</param>
+        /// <param name="pktList">Reveal packet list containing the commands and/or feedbacks to be sent</param>
+        public void SendRevealMessageOverRevelation(ConnectedApplianceInfo connectedAppliance, List<RevealPacket> pktList)
+        {
+            WifiLocal.SendRevelationMessage(connectedAppliance, pktList.ToRevelation());
+        }
+
+        /// <summary>
+        /// Send a Reveal Message over the Mqtt connection.
+        /// Use WifiLocal.ConnectedAppliances to get the list of current scanned and/or connected appliances.
+        /// </summary>
+        /// <param name="connectedAppliance">The connected Applicance</param>
+        /// <param name="pkt">Reveal packet containing the command or feedback to be sent</param>
+        public void SendRevealMessageOverMQTT(ConnectedApplianceInfo connectedAppliance, RevealPacket pkt)
+        {
+            WifiLocal.SendMqttMessage(connectedAppliance, pkt.ToMqtt());
+        }
+
+        /// <summary>
+        /// Send a Reveal Message over the Mqtt connection.
+        /// Use WifiLocal.ConnectedAppliances to get the list of current scanned and/or connected appliances.
+        /// </summary>
+        /// <param name="connectedAppliance">The connected Applicance</param>
+        /// <param name="destination">Address to send the message</param>
+        /// <param name="api">Reveal Api</param>
+        /// <param name="opcode">Reveal Opcode</param>
+        /// <param name="isFeedback">Set true if the message is a feedback</param>
+        /// <param name="payload">payload bytes</param>
+        public void SendRevealMessageOverMQTT(ConnectedApplianceInfo connectedAppliance, byte destination, byte api, byte opcode, bool isFeedback, byte[] payload)
+        {
+            RevealPacket pkt = new RevealPacket(api, opcode, 0, destination, isFeedback, payload);
+            WifiLocal.SendMqttMessage(connectedAppliance, pkt.ToMqtt());
+        }
+
+        #endregion
+
+        private void Venom_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
